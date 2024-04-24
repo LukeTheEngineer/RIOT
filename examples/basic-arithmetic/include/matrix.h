eb4d0a529410cb1d1412d1e003de8b05d066b531
LukeTheEngineer <LukasJacksonEG@gmail.com>
2024-04-23 20:47:11 -0500

    Added header guards

--- examples/basic-arithmetic/include/matrix.h
+++ examples/basic-arithmetic/include/matrix.h
@@ -1,3 +1,7 @@
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+
 #define ROWS_A 2
 #define COLS_A 3
 #define ROWS_B 3
@@ -6,3 +10,5 @@
 void matrix_multiply(int A[ROWS_A][COLS_A], int B[ROWS_B][COLS_B], int result[ROWS_A][COLS_B]);
 
 void print_matrix(int rows, int cols, int matrix[rows][cols]);
+
+#endif /*> MATRIX_H_ <*/
